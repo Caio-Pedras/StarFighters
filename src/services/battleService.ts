26057cc00b22142c06c7dc3c43a001ba9cf90013
CaioPedras <caio.vp@gmail.com>
2022-08-31 21:46:56 -0300

    remove teste

--- src/services/battleService.ts
+++ src/services/battleService.ts
@@ -2,28 +2,28 @@ import axios from "axios";
 import * as fighterRepository from "../repositories/fighterRepository.js";
 
 
-async function getFighterInfo(username: string)   {
-  const { data }  = await axios.get(`https://api.github.com/users/${username}/repos`);
-  
-  return data ;
- 
+async function getFighterInfo(username: string) {
+  const { data } = await axios.get(`https://api.github.com/users/${username}/repos`);
+
+  return data;
+
 }
 
 async function getFighter(username: string) {
-  const {rows: fighters }= await fighterRepository.findByUsername(username);
+  const { rows: fighters } = await fighterRepository.findByUsername(username);
   const fighter = fighters[0]
   if (!fighter) {
     await fighterRepository.insertFighter(username);
-    const {rows : createdFighter} = await fighterRepository.findByUsername(username);
+    const { rows: createdFighter } = await fighterRepository.findByUsername(username);
     console.log(createdFighter, 'testeee')
     return { id: createdFighter[0].id, username, wins: 0, losses: 0, draws: 0 };
   }
-  
+
   return fighter;
 }
 function getFighterStarCount(fighterRepos: any[]) {
 
-  const repoStars: number[] = fighterRepos.map((repo) => {return repo.stargazers_count});
+  const repoStars: number[] = fighterRepos.map((repo) => { return repo.stargazers_count });
   if (repoStars.length === 0) return 0;
   const result = repoStars.reduce((current: number, sum: number) => sum + current);
   return result
@@ -33,12 +33,12 @@ export async function battle(firstUser: string, secondUser: string) {
   const firstUserInfo = await getFighterInfo(firstUser);
   const secondUserInfo = await getFighterInfo(secondUser);
 
-  if(firstUserInfo.length===0 || secondUserInfo.length===0){
-    throw {type:"NotFound", message:"Both users must be registered on github"}
+  if (firstUserInfo.length === 0 || secondUserInfo.length === 0) {
+    throw { type: "NotFound", message: "Both users must be registered on github" }
   }
-  const firstFighter  = await getFighter(firstUser);
-  const secondFighter= await getFighter(secondUser);
-  
+  const firstFighter = await getFighter(firstUser);
+  const secondFighter = await getFighter(secondUser);
+
 
   const firstUserStarCount = getFighterStarCount(firstUserInfo);
   const secondUserStarCount = getFighterStarCount(secondUserInfo);
@@ -52,14 +52,13 @@ export async function battle(firstUser: string, secondUser: string) {
 }
 
 async function getBattleResult(firstFighter: any, secondFighter: any, firstUserStarCount: number, secondUserStarCount: number) {
- 
+
   if (firstUserStarCount > secondUserStarCount) {
     await updateWinnerAndLoserStats(firstFighter.id, secondFighter.id);
     return {
       winner: firstFighter.username,
       loser: secondFighter.username,
       draw: false,
-      teste:"caso1"
     };
   }
 
@@ -69,7 +68,6 @@ async function getBattleResult(firstFighter: any, secondFighter: any, firstUserS
       winner: secondFighter.username,
       loser: firstFighter.username,
       draw: false,
-      teste:"caso2"
     };
   }
 
